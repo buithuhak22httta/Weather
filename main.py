50b26a06d153da5f6990a3089b1f7dd963d31fdc
Bùi Thu Hà <90203839+buithuhak22httta@users.noreply.github.com>
2023-05-10 22:49:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 import pandas as pd
-from google.cloud import bigquery
+# from google.cloud import bigquery
 
-client = bigquery.Client()
-table_id="sacred-alpha-363907.Ver_1.weather"
+# client = bigquery.Client()
+# table_id="sacred-alpha-363907.Ver_1.weather"
 def weather():
     api_url = "http://api.openweathermap.org/data/2.5/forecast?id=1581130&lang=vi&units=metric&appid=07a92bdc234b052aee22ad1f7151f201"
     request = requests.get(api_url)
@@ -20,11 +20,12 @@ def weather():
         temp_min = main["temp_min"]
         temp_max = main["temp_max"]
         my_list=(time, description, temp, temp_min, temp_max)
-        rows_to_insert.append(my_list)
-    table = client.get_table(table_id)
-    errors = client.insert_rows(table, rows_to_insert)
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
+        print(my_list)
+#         rows_to_insert.append(my_list)
+#     table = client.get_table(table_id)
+#     errors = client.insert_rows(table, rows_to_insert)
+#     if errors == []:
+#         print("New rows have been added.")
+#     else:
+#         print("Encountered errors while inserting rows: {}".format(errors))
 weather()
